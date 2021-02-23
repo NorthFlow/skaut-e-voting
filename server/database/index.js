9a9bdb0a4b108385618b2d7fadf9187375434c98
MichMat97 <matia.michal.97@gmail.com>
2021-02-23 01:14:01 +0100

    added some SELECT, TODO on line 104

--- server/database/index.js
+++ server/database/index.js
@@ -62,11 +62,50 @@ userdb.login = (login, password) => {
 // --- voting sequence
 userdb.getAllQuestions = () => {
     return new Promise((resolve, reject) => {
-        connection.query(`SELECT * FROM questions`, (err, results) => {
+        connection.query(`SELECT id_question AS "ID",wording AS "Znenie otázky",name AS "Názov hlasovania" FROM questions JOIN voting USING (id_voting) ORDER BY id_question ASC`, (err, results) => {
             if (err) {
                 return reject(err);
             } 
             return resolve(results);
+        });
+    })
+
+}
+// --- return list of
+userdb.getAllVotings = () => {
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT id_voting AS "ID",name AS "Názov hlasovania", CASE accept_answ WHEN 0 THEN 'Nie' WHEN 1 THEN 'Áno' END AS "Hlasovanie spustené", CASE secret WHEN 0 THEN 'Nie' WHEN 1 THEN 'Áno' END AS "Tajná voľba" FROM voting ORDER BY id_voting ASC `, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(results);
+        });
+    })
+
+}
+
+// --- check - can vote? 0->False
+userdb.checkVotingOpened = (voting_id) => {
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT accept_answ FROM voting WHERE id_voting = ? `,[voting_id], (err, result) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(result);
+        });
+    })
+
+}
+
+// --- check - voted yet? 0->False
+userdb.checkVoted = (voting_id,user_id) => {
+    return new Promise((resolve, reject) => {
+        //TODO neviem ako sa odkazuje pri viacerých parametroch tak toto prosím pozri
+        connection.query(`SELECT COUNT(*) FROM user-voting WHERE id_voting = ? AND id_user = ?`,[voting_id,user_id], (err, result) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(result);
         });
     })
 
